0d1f1661487896fe3974c40e72ec824ae2f97ebf
Linus Quan <Linus@Linuss-MacBook-Pro.local>
2016-12-07 16:54:43 +1100

    $scope service, dependency injection

--- pages/app.js
+++ pages/app.js
@@ -2,9 +2,27 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
+angularApp.controller('mainController', function ($scope) {
+    $scope.name = "Li Quan";
+    $scope.job = "Good Software Developer";
+    $scope.getName = function () {
+        return "Tell me the name";
+    }
 
-}]);
+    $scope.getJob = function () {
+        // do not put $scope as function param here, this will not be dependency injection
+        return $scope.job;
+    }
+
+    console.log($scope); // variable name created the in $scope
+    console.log($scope.getName());
+    console.log($scope.getJob());
+});
+
+/*
+* How does angular do Dependency injection?
+* The controller function sees "$scope" and knows that, create the $scope and inject to function
+ */
 
 
 
