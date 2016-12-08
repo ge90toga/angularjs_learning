550c1555ff618ad5813d2b7ebf8875ca4c2f6112
Linus Quan <Linus@Linuss-MacBook-Pro.local>
2016-12-08 20:13:04 +1100

    angular router

--- seed/app/app.js
+++ seed/app/app.js
@@ -1,7 +1,25 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [])
-    .controller('mainController',["$scope","$location","$log",function ($scope,$location,$log) {
-      $log.info($location.path()); // try visiting by localhost/index.html#/bookmark/1
-    }]);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',{
+            templateUrl:'pages/main.html',
+            controller:'mainController'
+        })
+        .when('/second',{
+            templateUrl:'pages/second.html',
+            controller:'secondController'
+        });
+
+});
+
+myApp.controller('mainController',["$scope",function ($scope) {
+    $scope.name = "Main";
+}]);
+
+myApp.controller('secondController',["$scope",function ($scope) {
+    $scope.name = "Second";
+}]);
