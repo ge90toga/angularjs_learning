1424e3fc4c43075d0012ff9ef666bc119d005c01
Linus Quan <Linus@Linuss-MacBook-Pro.local>
2016-12-08 12:05:51 +1100

    $scope watchers and digest cycle

--- pages/app.js
+++ pages/app.js
@@ -1,9 +1,37 @@
 var myApp = angular.module('myApp', []); // modules from other files (not angular.js)
 
-myApp.controller('mainController', ["$scope","$log", function ($scope, $log) {
-    // this version of DI will be magnification proof
-    // the order matters, the service injection and function parameter has to be matched
-    // function has to be at the last element of the array
+myApp.controller('mainController', ["$scope","$filter","$log", function ($scope,$filter,$log) {
+    $scope.handle = ''; // declaration here is optional since the view has ng-model directive
+
+    $scope.lowercaseHandle = function(){
+        return $filter("lowercase")($scope.handle);
+    }
+
+    // manually add watches
+    // $scope.$watch('handle',function (oldValue,newValue) {
+    //     $log.info('changed old: '+ oldValue+" new: "+newValue);
+    // })
+
+    // a way might make the scope watcher unaware of changes because
+    // this traditional js code runs outside of the scope context
+
+    // setTimeout(function () {
+    //     $scope.handle = 'new value';
+    //     $log.info('changed!');
+    // },3000);
+
+    // use scope apply to make the change affect the $scope
+    // setTimeout(function () {
+    //     $scope.$apply(function () {
+    //         $scope.handle = "hehe";
+    //     })
+    //     $log.info('changed!');
+    // },3000);
+
+    // or use the $time service
+
+
+
 }]);
 
 // This is the compressed version, angular can inject it only if you keep the order correct
--- pages/note.md
+++ pages/note.md
@@ -1,1 +1,7 @@
-Dependency Injection survived code minification
+### Watchers and digest loop
+- angular watches: anytime you put a var or function through 
+binding or {{}}, angular put it into watch list and listen
+to the change of it by running digest loop.
+
+~/scope: a example of using $scope and rootScope
+
